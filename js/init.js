4e41733703f9f6234bb323612b30da85951953dd
memoorte <74221690+memoorte@users.noreply.github.com>
2021-04-13 21:16:44 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
 // @ts-ignore
-var firebaseConfig = {
-    apiKey: "AIzaSyAj6r2-G9UHGs0uZUAXvmQE_3KrfUu90Ds",
+firebase.initializeApp({  
+  apiKey: "AIzaSyAj6r2-G9UHGs0uZUAXvmQE_3KrfUu90Ds",
     authDomain: "pwamemo.firebaseapp.com",
     projectId: "pwamemo",
     storageBucket: "pwamemo.appspot.com",
     messagingSenderId: "794991437818",
     appId: "1:794991437818:web:2030dfeb350fba8dfb2643"
-  };
+});
